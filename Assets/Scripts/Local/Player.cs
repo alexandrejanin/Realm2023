5217a877678d5864d443c3aa9ebc4a8f373dd738
KillingBanana <killingbanana@gmail.com>
2018-01-24 11:38:52 +0100

    Fixed button frame position

--- Assets/Scripts/Local/Player.cs
+++ Assets/Scripts/Local/Player.cs
@@ -7,8 +7,7 @@ using UnityEngine.UI;
 public class Player : MonoBehaviour {
 	private static Character character;
 
-	private const float turnsPerSecond = 4;
-	public const float turnDuration = 1 / turnsPerSecond; //Duration of a turn in seconds
+	private const float TurnDuration = 1f / 4f; //Duration of a turn in seconds
 	private static float turnTimer;
 	private static bool paused;
 
@@ -36,7 +35,7 @@ public class Player : MonoBehaviour {
 				turnTimer -= Time.deltaTime;
 				if (turnTimer <= 0 && !paused) {
 					ObjectManager.TakeTurn();
-					turnTimer = turnDuration;
+					turnTimer = TurnDuration;
 				}
 			} else {
 				turnTimer = 0;
@@ -78,24 +77,31 @@ public class Player : MonoBehaviour {
 				if (Input.GetKeyDown(KeyCode.Keypad1)) {
 					MoveTowards(Coord.Back + Coord.Left);
 				}
+
 				if (Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.DownArrow)) {
 					MoveTowards(Coord.Back);
 				}
+
 				if (Input.GetKeyDown(KeyCode.Keypad3)) {
 					MoveTowards(Coord.Back + Coord.Right);
 				}
+
 				if (Input.GetKeyDown(KeyCode.Keypad4) || Input.GetKeyDown(KeyCode.LeftArrow)) {
 					MoveTowards(Coord.Left);
 				}
+
 				if (Input.GetKeyDown(KeyCode.Keypad6) || Input.GetKeyDown(KeyCode.RightArrow)) {
 					MoveTowards(Coord.Right);
 				}
+
 				if (Input.GetKeyDown(KeyCode.Keypad7)) {
 					MoveTowards(Coord.Forward + Coord.Left);
 				}
+
 				if (Input.GetKeyDown(KeyCode.Keypad8) || Input.GetKeyDown(KeyCode.UpArrow)) {
 					MoveTowards(Coord.Forward);
 				}
+
 				if (Input.GetKeyDown(KeyCode.Keypad9)) {
 					MoveTowards(Coord.Forward + Coord.Right);
 				}
@@ -103,7 +109,7 @@ public class Player : MonoBehaviour {
 		}
 	}
 
-	public void MoveTowards(Coord direction) {
+	private static void MoveTowards(Coord direction) {
 		QuaternionInt rotation = new QuaternionInt(0, Mathf.RoundToInt(Camera.transform.eulerAngles.y / 45f), 0);
 		direction = rotation * direction;
 		Node node = GetValidNode(direction);
@@ -119,6 +125,7 @@ public class Player : MonoBehaviour {
 			Node targetNode = NodeGrid.GetNeighbor(character.position, direction);
 			if (targetNode != null) return targetNode;
 		}
+
 		return null;
 	}
 
@@ -129,21 +136,20 @@ public class Player : MonoBehaviour {
 	public static void DisplayInteractions(string title, ICollection<Interaction> interactions) {
 		if (interactions == null || interactions.Count == 0) return;
 
-		PrefabManager prefabManager = GameController.PrefabManager;
-
-		ButtonsFrame frame = Instantiate(prefabManager.buttonsFramePrefab, Input.mousePosition, Quaternion.identity, canvasTransform);
+		ButtonsFrame frame = Instantiate(GameController.PrefabManager.buttonsFramePrefab, canvasTransform);
+		frame.transform.position = Input.mousePosition;
 		frame.GetComponentInChildren<Text>().text = title;
 		Transform parent = frame.buttonsParent.transform;
 
 		foreach (Interaction interaction in interactions) {
-			Button button = Instantiate(prefabManager.buttonPrefab, parent);
+			Button button = Instantiate(GameController.PrefabManager.buttonPrefab, parent);
 			button.GetComponentInChildren<Text>().text = interaction.name;
 			button.onClick.AddListener(() => interaction.action.Invoke());
 			button.onClick.AddListener(() => Destroy(frame.gameObject));
 			if (interaction.skipTurn) button.onClick.AddListener(ObjectManager.TakeTurn);
 		}
 
-		frame.SetSize(prefabManager.buttonPrefab.GetComponent<RectTransform>().sizeDelta.y + 1);
+		frame.SetSize(GameController.PrefabManager.buttonPrefab.GetComponent<RectTransform>().sizeDelta.y + 1);
 		interactionsMenu = frame.gameObject;
 	}
 
